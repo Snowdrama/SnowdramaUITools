857956ec518c55c6147bfb555c4bf0f15cb73e4f
John 'Snow' Close <SnowdramaGames@gmail.com>
2023-05-09 01:07:20 -0400

    reorganizing things

---
+++ UI/Layout/UIGridGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Snowdrama.UI
+{
+    /// <summary>
+    /// This class fits all the children into the current RectTransform
+    /// </summary>
+    [ExecuteInEditMode]
+    public class UIGridGroup : SnowUI
+    {
+
+        [Header("Grid Settings")]
+        public int numberOfRows = 0;
+        public int numberOfColumns = 0;
+        void LateUpdate()
+        {
+            if (transform.childCount != children.Count || forceUpdate)
+            {
+                forceUpdate = false;
+                CollectChildren();
+                CalculateGrid(children.Count, numberOfColumns, numberOfRows);
+                ProcessChildren();
+            }
+        }
+    }
+}
